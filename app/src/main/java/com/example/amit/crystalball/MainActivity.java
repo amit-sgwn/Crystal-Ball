81ace85f7a47560e72b731ed58de56e4fd7d3c62
Amit Kumar <y13uc022@lnmiit.ac.in>
2016-06-15 20:53:45 +0530

    Added sound to Button ans tween animation to textview

--- app/src/main/java/com/example/amit/crystalball/MainActivity.java
+++ app/src/main/java/com/example/amit/crystalball/MainActivity.java
@@ -1,43 +1,69 @@
 package com.example.amit.crystalball;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
     private CrystalBall ball = new CrystalBall();
+    TextView mGetAnswer;
+    ImageView mCrystalball;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
-        final TextView text;
-        Button btn;
-        text =(TextView)findViewById(R.id.textView);
+        mGetAnswer =(TextView)findViewById(R.id.textView);
         btn = (Button)findViewById(R.id.button);
-        assert btn != null;
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                text.setText(ball.getAnswer());
+                mGetAnswer.setText(ball.getAnswer());
 
                 animationCrystalBall();
+                answerAnimation();
+                playSound();
             }
         });
     }
 
+
+
     private void animationCrystalBall(){
 
-        ImageView crystalball = (ImageView)findViewById(R.id.imageView);
-        crystalball.setImageResource(R.drawable.ball_animation);
-        AnimationDrawable animation = (AnimationDrawable) crystalball.getDrawable();
+        mCrystalball = (ImageView)findViewById(R.id.imageView);
+        mCrystalball.setImageResource(R.drawable.ball_animation);
+        AnimationDrawable animation = (AnimationDrawable) mCrystalball.getDrawable();
         animation.start();
+    }
+    private void answerAnimation() {
+        AlphaAnimation fadeanim = new AlphaAnimation(0,1);
+        fadeanim.setDuration(1500);
+        mGetAnswer.setAnimation(fadeanim);
+
+    }
+
+    private void playSound(){
+        MediaPlayer player = MediaPlayer.create(this,R.raw.crystal_ball);
+        player.start();
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+
     }
 
     @Override
